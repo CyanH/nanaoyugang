6157f61410958d3216fbb293182297ee31e26789
Cyan <chenhaiyanccc@163.com>
2023-01-04 11:51:00 +0800

    渔民管理

--- src/views/people/leftDrawer.vue
+++ src/views/people/leftDrawer.vue
@@ -11,19 +11,19 @@
         <div class="trawler-overview-item">
           <div class="trawler-overview-wrapper flex">
             <img src="@/assets/image/people/icon-crew-arrival.png" class="item-icon" />
-            <div class="content"><span class="label">在港船员</span><span class="num">123</span></div>
+            <div class="content"><span class="label">保险过期人员</span><span class="num">3</span></div>
           </div>
         </div>
         <div class="trawler-overview-item">
           <div class="trawler-overview-wrapper flex">
             <img src="@/assets/image/people/icon-crew-arrival.png" class="item-icon" />
-            <div class="content"><span class="label">在外作业人员</span><span class="num">720</span></div>
+            <div class="content"><span class="label">证书过期人员</span><span class="num">1</span></div>
           </div>
         </div>
         <div class="trawler-overview-item">
           <div class="trawler-overview-wrapper flex">
             <img src="@/assets/image/people/icon-crew-arrival.png" class="item-icon" />
-            <div class="content"><span class="label">超长作业人员</span><span class="num">45</span></div>
+            <div class="content"><span class="label">超长作业人员</span><span class="num">8</span></div>
           </div>
         </div>
       </div>
@@ -31,9 +31,41 @@
       <div class="list">
         <div class="top">
           <v-title title="培训记录"></v-title>
+          <div class="cont">
+            <div class="nav">
+              <div>培训名称</div>
+              <div>培训类型</div>
+              <div>培训时间</div>
+            </div>
+            <div class="content">
+              <vue3-seamless-scroll :list="state.recordList" :step="0.3" :hover="true" :limitScrollNum="6">
+                <div class="row" v-for="(item, index) in state.recordList" :key="index">
+                  <div>{{ item.name }}</div>
+                  <div>{{ item.type }}</div>
+                  <div>{{ item.date }}</div>
+                </div>
+              </vue3-seamless-scroll>
+            </div>
+          </div>
         </div>
         <div class="bottom">
           <v-title title="船员违规"></v-title>
+          <div class="cont">
+            <div class="nav">
+              <div>姓名</div>
+              <div>违规类型</div>
+              <div>违规时间</div>
+            </div>
+            <div class="content">
+              <vue3-seamless-scroll :list="state.ruleList" :step="0.3" :hover="true" :limitScrollNum="6">
+                <div class="row" v-for="(item, index) in state.ruleList" :key="index">
+                  <div>{{ item.name }}</div>
+                  <div>{{ item.type }}</div>
+                  <div>{{ item.date }}</div>
+                </div>
+              </vue3-seamless-scroll>
+            </div>
+          </div>
         </div>
       </div>
     </v-card>
@@ -41,7 +73,86 @@
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue';
 import { CountTo } from 'vue3-count-to';
+import { Vue3SeamlessScroll } from 'vue3-seamless-scroll';
+
+const state = reactive({
+  recordList: [
+    {
+      name: '港口卫生',
+      type: '日常培训',
+      date: '2022-12-21',
+    },
+    {
+      name: '海上大风安全自救',
+      type: '安全生产第一课',
+      date: '2022-12-24',
+    },
+    {
+      name: '海上大风翻船自救',
+      type: '安全生产第一课',
+      date: '2022-12-25',
+    },
+    {
+      name: '船员行为规范',
+      type: '日常培训',
+      date: '2022-12-28',
+    },
+    {
+      name: '开海管理',
+      type: '日常培训',
+      date: '2022-12-31',
+    },
+    {
+      name: '海上台风来袭',
+      type: '安全生产第一课',
+      date: '2023-1-3',
+    },
+    {
+      name: '海上船舶机械故障',
+      type: '安全生产第一课',
+      date: '2023-1-5',
+    },
+  ],
+  ruleList: [
+    {
+      name: '张结清',
+      type: '证书违规',
+      date: '2022-12-21',
+    },
+    {
+      name: '张执满',
+      type: '证书违规',
+      date: '2022-12-24',
+    },
+    {
+      name: '张华',
+      type: '行为违规',
+      date: '2022-12-25',
+    },
+    {
+      name: '何三珠',
+      type: '行为违规',
+      date: '2022-12-28',
+    },
+    {
+      name: '张执发',
+      type: '证书违规',
+      date: '2022-12-31',
+    },
+    {
+      name: '黄正',
+      type: '行为违规',
+      date: '2023-1-3',
+    },
+    {
+      name: '何伍毅',
+      type: '证书违规',
+      date: '2023-1-5',
+    },
+  ],
+});
 </script>
 
 <style lang="scss" scoped>
@@ -115,6 +226,73 @@ import { CountTo } from 'vue3-count-to';
 
   .top {
     margin-bottom: 20px;
+  }
+
+  .cont {
+    height: calc(100% - 50px);
+    margin-top: 12px;
+
+    .nav {
+      width: 100%;
+      display: flex;
+      height: 34px;
+      line-height: 34px;
+      font-size: 16px;
+      background: rgba(19, 39, 165, 0.5);
+      color: rgba(255, 255, 255, 0.8);
+      text-align: center;
+
+      :nth-child(1) {
+        width: 30%;
+      }
+      :nth-child(2) {
+        width: 35%;
+      }
+      :nth-child(3) {
+        width: 35%;
+      }
+    }
+    .content {
+      width: 100%;
+      height: calc(100% - 34px);
+      overflow: hidden;
+
+      .row {
+        width: 100%;
+        height: 38px;
+        align-items: center;
+        color: #ffffff;
+        font-size: 16px;
+        text-align: center;
+        display: flex;
+
+        &:nth-child(even) {
+          background-color: rgba(19, 39, 165, 0.2);
+        }
+
+        :nth-child(1) {
+          width: 30%;
+        }
+        :nth-child(2) {
+          width: 35%;
+        }
+        :nth-child(3) {
+          width: 35%;
+        }
+      }
+
+      .wkg {
+        color: #ff8251;
+      }
+
+      .sgz {
+        color: #00f6ff;
+      }
+
+      .ywg {
+        color: #22ff51;
+      }
+    }
   }
 }
 </style>
--- src/views/people/rightDrawer.vue
+++ src/views/people/rightDrawer.vue
@@ -2,84 +2,193 @@
   <v-drawer direction="right" hasArrow>
     <v-card>
       <div class="top">
-        <v-title title="供需物流"></v-title>
-        <div class="flex-between wrapper">
-          <div>
-            <div class="num">1210</div>
-            <div class="label">供货统计/吨</div>
-          </div>
-          <div>
-            <div class="num">820</div>
-            <div class="label">求购统计/吨</div>
-          </div>
-          <div>
-            <div class="num">121</div>
-            <div class="label">车辆需求统计</div>
-          </div>
-          <div>
-            <div class="num">107</div>
-            <div class="label">车辆供应统计</div>
-          </div>
+        <v-title title="船员动态"></v-title>
+        <div class="flex wrapper">
+          <img src="@/assets/image/people/icon-people.png" class="icon" />
+          <span class="label">当前作业人员</span>
+          <count-to :startVal="0" :endVal="821" :duration="3000" class="num"></count-to>
+          <span>人</span>
         </div>
       </div>
 
       <div class="bottom">
-        <div class="bt-top">
-          <v-title title="渔获电子交易市场统计"></v-title>
-
-          <div class="chart"></div>
-        </div>
-
-        <div class="bt-bottom">
-          <v-title title="价格走势分析"></v-title>
-
-          <div class="chart"></div>
+        <v-title title="船员进出港记录"></v-title>
+        <div class="cont">
+          <div class="nav">
+            <div>姓名</div>
+            <div>渔船名称</div>
+            <div>进出港</div>
+            <div>时间</div>
+          </div>
+          <div class="content">
+            <vue3-seamless-scroll :list="state.recordList" :step="0.3" :hover="true" :limitScrollNum="7">
+              <div class="row" v-for="(item, index) in state.recordList" :key="index">
+                <div>{{ item.name }}</div>
+                <div>{{ item.shipName }}</div>
+                <div>{{ item.type }}</div>
+                <div>{{ item.date }}</div>
+              </div>
+            </vue3-seamless-scroll>
+          </div>
         </div>
       </div>
     </v-card>
   </v-drawer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { CountTo } from 'vue3-count-to';
+import { Vue3SeamlessScroll } from 'vue3-seamless-scroll';
+
+const state = reactive({
+  recordList: [
+    {
+      name: '郭艳珍',
+      shipName: '粤汕渔05417',
+      type: '出港',
+      date: '2022-1-4 03:40:20',
+    },
+    {
+      name: '郭永雄',
+      shipName: '粤汕渔02356',
+      type: '出港',
+      date: '2022-1-4 04:12:12',
+    },
+    {
+      name: '郭锦荣',
+      shipName: '粤汕渔01388',
+      type: '出港',
+      date: '2022-1-4 14:44:24',
+    },
+    {
+      name: '陈键潮',
+      shipName: '粤汕渔19582',
+      type: '进港',
+      date: '2022-1-4 10:44:43',
+    },
+    {
+      name: '黄润胜',
+      shipName: '粤汕渔05417',
+      type: '进港',
+      date: '2022-1-4 13:12:45',
+    },
+    {
+      name: '何虎山',
+      shipName: '粤汕渔15740',
+      type: '进港',
+      date: '2022-1-4 15:32:12',
+    },
+    {
+      name: '王富贵',
+      shipName: '粤汕渔84257',
+      type: '进港',
+      date: '2022-1-4 20:53:42',
+    },
+  ],
+});
+</script>
 
 <style lang="scss" scoped>
 .top {
-  padding-bottom: 20px;
+  height: 60%;
   .wrapper {
-    margin-top: 20px;
-    .num {
-      width: 60px;
-      height: 60px;
-      font-size: 16px;
-      background: url('@/assets/image/catch/icon-gx.png') center no-repeat;
-      background-size: 100% 100%;
-      text-align: center;
-      line-height: 60px;
-      margin: auto;
+    margin: 15px 0;
+    font-size: 18px;
+    color: #e3f0ff;
+
+    .icon {
+      width: 28px;
+      margin-top: -5px;
     }
 
     .label {
-      margin-top: 10px;
-      font-size: 14px;
+      margin-left: -14px;
+      margin-right: 20px;
+    }
+
+    .num {
+      font-size: 24px;
+      font-style: oblique;
+      color: #0bf;
+      margin-right: 12px;
     }
   }
 }
 
 .bottom {
-  height: calc(100% - 164px);
+  height: 40%;
 
-  .bt-top,
-  .bt-bottom {
-    height: calc(50% - 10px);
-  }
+  .cont {
+    height: calc(100% - 50px);
+    margin-top: 12px;
+    .nav {
+      width: 100%;
+      display: flex;
+      height: 34px;
+      line-height: 34px;
+      font-size: 16px;
+      background: rgba(19, 39, 165, 0.5);
+      color: rgba(255, 255, 255, 0.8);
+      text-align: center;
 
-  .bt-top {
-    margin-bottom: 20px;
-  }
+      :nth-child(1) {
+        width: 18%;
+      }
+      :nth-child(2) {
+        width: 25%;
+      }
+      :nth-child(3) {
+        width: 18%;
+      }
+      :nth-child(4) {
+        width: 38%;
+      }
+    }
+    .content {
+      width: 100%;
+      height: calc(100% - 34px);
+      overflow: hidden;
 
-  .chart {
-    height: calc(100% - 55px);
-    margin-top: 17px;
+      .row {
+        width: 100%;
+        height: 38px;
+        align-items: center;
+        color: #ffffff;
+        font-size: 16px;
+        text-align: center;
+        display: flex;
+
+        &:nth-child(even) {
+          background-color: rgba(19, 39, 165, 0.2);
+        }
+
+        :nth-child(1) {
+          width: 18%;
+        }
+        :nth-child(2) {
+          width: 26%;
+        }
+        :nth-child(3) {
+          width: 18%;
+        }
+        :nth-child(4) {
+          width: 38%;
+        }
+      }
+
+      .wkg {
+        color: #ff8251;
+      }
+
+      .sgz {
+        color: #00f6ff;
+      }
+
+      .ywg {
+        color: #22ff51;
+      }
+    }
   }
 }
 </style>
