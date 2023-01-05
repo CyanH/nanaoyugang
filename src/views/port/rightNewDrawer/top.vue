a19cf8e4c69eadeff08e08250ebbdf9711c4efce
Cyan <chenhaiyanccc@163.com>
2023-01-05 14:54:14 +0800

    渔港管理

--- src/views/port/rightNewDrawer/top.vue
+++ src/views/port/rightNewDrawer/top.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="top">
-      <div class="one">
-          <div class="title">
-              <div class="left">
-                <img src="@/assets/image/card/o1.png" alt="">
-                <span class="span">水产交易</span>
-              </div>
-              <div class="right">
-                  <count-to :startVal="0" :endVal="8" :duration="3000" class="num"></count-to>
-                  <span class="dw">万元</span>
-              </div>
-          </div>
-          <div class="echat">
-              <div class="chart" ref="chartEle_all"></div>
-          </div>
+    <div class="one">
+      <div class="title">
+        <div class="left">
+          <img src="@/assets/image/card/o1.png" alt="" />
+          <span class="span">水产交易</span>
+        </div>
+        <div class="right">
+          <count-to :startVal="0" :endVal="8" :duration="3000" class="num"></count-to>
+          <span class="dw">万元</span>
+        </div>
       </div>
-      <div class="one two">
-          <div class="echat1">
-              <product-middle></product-middle>
-          </div>
+      <div class="echat">
+        <div class="chart" ref="chartEle_all"></div>
       </div>
+    </div>
+    <div class="one two">
+      <div class="echat1">
+        <product-middle></product-middle>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import * as echarts from 'echarts';
-import { onMounted, ref, reactive,onUnmounted } from 'vue';
+import { onMounted, ref, onUnmounted } from 'vue';
 import productMiddle from './productMiddle.vue';
 import { CountTo } from 'vue3-count-to';
 // 项目、资金折线图
@@ -35,95 +35,78 @@ import { loadChart } from '../chart/top';
 let myChar_all: echarts.ECharts;
 let chartEle_all = ref<HTMLDivElement | null>(null);
 
-const state = reactive({
-  list: [
-    {
-      title: '今日预约游客量',
-      money: 0,
-      bl: 0,
-      color: '#5782FF',
-    },
-    {
-      title: '今日游船使用量',
-      money: 0,
-      bl: 0,
-      color: '#FFB852',
-    },
-  ] as any,
-});
-
 onMounted(() => {
   myChar_all = echarts.init(chartEle_all.value as HTMLDivElement);
   window.addEventListener('resize', () => {
-      myChar_all.resize();
+    myChar_all.resize();
   });
   getData();
 });
 onUnmounted(() => {
   window.addEventListener('resize', () => {
-      myChar_all.resize();
+    myChar_all.resize();
   });
-})
+});
 const getData = () => {
-  let data = [10000,50000,65555,97777,84444,7111,10222]
-  let date = ['1-01','1-02','1-03','1-04','1-05','1-06','1-07']
-  loadChart(myChar_all,data,date,'水产','元')
+  let data = [76000, 72658, 75555, 87777, 84444, 71111, 80222];
+  let date = ['1-01', '1-02', '1-03', '1-04', '1-05', '1-06', '1-07'];
+  loadChart(myChar_all, data, date, '水产', '元');
 };
 </script>
 
 <style lang="scss" scoped>
-.top{
+.top {
   width: 100%;
   height: calc(55% - 16px);
   margin-bottom: 16px;
-  .one{
-      height: 55%;
-      .title{
-          height: 40px;
-          margin-top: 16px;
-          // background-color: yellow;
-          display: flex;
-          .left{
-              font-size: 20px;
-              // width: 180px;
-              display: flex;
-              .span{
-                margin-right: 4px;
-                color: #e3f0ff;
-                font-size: 18px;
-                line-height: 1;
-                margin-left: -20px;
-                padding-top: 15px;
-              }
-          }
-          .right{
-              flex: 1;
-              .num{
-                  font-size: 30px;
-                  padding-right: 5px;
-                  color: #0bf;
-              }
-          }
-      }
-      .echat{
-          width: 100%;
-          height: calc(100% - 56px);
-          .chart{
-              height: 100%;
-              width: 100%;
-          }
-      }
-      .echat1{
-        width: 100%;
-        height: 100%;
-        .chart{
-          height: 100%;
-          width: 100%;
+  .one {
+    height: 55%;
+    .title {
+      height: 40px;
+      padding-top: 16px;
+      // background-color: yellow;
+      display: flex;
+      .left {
+        font-size: 20px;
+        // width: 180px;
+        display: flex;
+        .span {
+          margin-right: 4px;
+          color: #e3f0ff;
+          font-size: 18px;
+          line-height: 1;
+          margin-left: -20px;
+          padding-top: 15px;
         }
       }
+      .right {
+        flex: 1;
+        .num {
+          font-size: 30px;
+          padding-right: 5px;
+          color: #0bf;
+        }
+      }
+    }
+    .echat {
+      width: 100%;
+      height: calc(100% - 56px);
+      .chart {
+        height: 100%;
+        width: 100%;
+      }
+    }
+    .echat1 {
+      width: 100%;
+      height: 100%;
+      .chart {
+        height: 100%;
+        width: 100%;
+      }
+    }
   }
-  .two{
-      height: 45%;
+  .two {
+    height: 45%;
   }
 }
 </style>
--- src/views/port/rightNewDrawer/topTwo.vue
+++ src/views/port/rightNewDrawer/topTwo.vue
@@ -1,242 +1,224 @@
 <template>
   <div class="top">
-      <div class="title">
-          <div class="left">
-              <img src="@/assets/image/card/o2.png" alt="">
-              <span class="span">港内安全巡查</span>
-          </div>
-          <div class="right">
-              <count-to :startVal="0" :endVal="12" :duration="3000" class="num"></count-to>
-              <span class="dw">次</span>
-          </div>
+    <div class="title">
+      <div class="left">
+        <img src="@/assets/image/card/o2.png" alt="" />
+        <span class="span">港内安全巡查</span>
       </div>
-      <div class="con">
-          <div class="nav">
-              <div>序号</div>
-              <div>类型</div>
-              <div>报警原因</div>
-              <div>时间</div>
-          </div>
-          <div class="content">
-              <vue3-seamless-scroll :list="state.projectList" :step="0.3" :hover="true" :limitScrollNum="11">
-                  <div class="row" v-for="(item, index) in state.projectList" :key="index">
-                      <div>{{ index + 1 }}</div>
-                      <el-tooltip placement="top">
-                          <template #content>{{ item.lx }}</template>
-                          <span>{{ item.lx }}</span>
-                      </el-tooltip>
-                      <el-tooltip placement="top">
-                          <template #content>{{ item.title }}</template>
-                          <span>{{ item.title }}</span>
-                      </el-tooltip>
-                      <el-tooltip placement="top">
-                          <template #content>{{ item.date }}</template>
-                          <span>{{ parseTime(item.date, '{y}-{m}-{d}') }}</span>
-                      </el-tooltip>
-                  </div>
-              </vue3-seamless-scroll>
-          </div>
+      <div class="right">
+        <count-to :startVal="0" :endVal="12" :duration="3000" class="num"></count-to>
+        <span class="dw">次</span>
       </div>
+    </div>
+    <div class="con">
+      <div class="nav">
+        <div>序号</div>
+        <div>类型</div>
+        <div>报警原因</div>
+        <div>时间</div>
+      </div>
+      <div class="content">
+        <vue3-seamless-scroll :list="state.projectList" :step="0.3" :hover="true" :limitScrollNum="11">
+          <div class="row" v-for="(item, index) in state.projectList" :key="index">
+            <div>{{ index + 1 }}</div>
+            <el-tooltip placement="top">
+              <template #content>{{ item.lx }}</template>
+              <span>{{ item.lx }}</span>
+            </el-tooltip>
+            <el-tooltip placement="top">
+              <template #content>{{ item.title }}</template>
+              <span>{{ item.title }}</span>
+            </el-tooltip>
+            <el-tooltip placement="top">
+              <template #content>{{ item.date }}</template>
+              <span>{{ parseTime(item.date, '{y}-{m}-{d}') }}</span>
+            </el-tooltip>
+          </div>
+        </vue3-seamless-scroll>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { CountTo } from 'vue3-count-to';
-import * as echarts from 'echarts';
 import { parseTime } from '@/utils/parseTime';
-import { onMounted, ref, reactive } from 'vue';
+import { reactive } from 'vue';
 import { Vue3SeamlessScroll } from 'vue3-seamless-scroll';
 // 项目、资金折线图
 
 const state = reactive({
-  list: [
+  projectList: [
     {
-      title: '今日预约游客量',
-      money: 0,
-      bl: 0,
-      color: '#5782FF',
+      title: '港内人员打架 ',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
     },
     {
-      title: '今日游船使用量',
-      money: 0,
-      bl: 0,
-      color: '#FFB852',
+      title: '明火作业',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '港内车辆相撞',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '港内人员打架 ',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '明火作业',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '港内车辆相撞',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '港内人员打架',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '明火作业',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '港内车辆相撞',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '港内人员打架',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '明火作业',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
+    },
+    {
+      title: '港内车辆相撞',
+      date: '2022-09-21 16:06:02',
+      lx: '港内安全',
     },
   ] as any,
-  projectList: [
-      {
-          title: '港内人员打架 ',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '明火作业',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '港内车辆相撞',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '港内人员打架 ',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '明火作业',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '港内车辆相撞',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '港内人员打架',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '明火作业',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '港内车辆相撞',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '港内人员打架',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '明火作业',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-      {
-          title: '港内车辆相撞',
-          date: '2022-09-21 16:06:02',
-          lx: '港内安全'
-      },
-  ] as any
 });
-
 </script>
 
 <style lang="scss" scoped>
-.top{
+.top {
   width: 100%;
-  height: 35%;
-  .title{
-      height: 40px;
-      margin-top: 16px;
-      // background-color: yellow;
+  height: calc(45% - 76px);
+  .title {
+    height: 40px;
+    padding-top: 16px;
+    display: flex;
+    .left {
+      font-size: 20px;
       display: flex;
-      .left{
-          font-size: 20px;
-          // width: 180px;
-          display: flex;
-          .span{
-              margin-right: 4px;
-              color: #e3f0ff;
-              font-size: 18px;
-              line-height: 1;
-              margin-left: -20px;
-              padding-top: 15px;
-          }
+      .span {
+        margin-right: 4px;
+        color: #e3f0ff;
+        font-size: 18px;
+        line-height: 1;
+        margin-left: -20px;
+        padding-top: 15px;
       }
-      .right{
-          flex: 1;
-          .num{
-              font-size: 30px;
-              padding-right: 5px;
-              color: #0bf;
-          }
+    }
+    .right {
+      flex: 1;
+      .num {
+        font-size: 30px;
+        padding-right: 5px;
+        color: #0bf;
       }
+    }
   }
-  .con{
-      height: calc(100% - 66px);
-      margin-top: 10px;
-      box-sizing: border-box;
-      .nav {
-          width: 100%;
-          display: flex;
-          height: 34px;
-          line-height: 34px;
-          font-size: 16px;
-          background: rgba(19, 39, 165, 0.5);
-          color: rgba(255, 255, 255, 0.8);
-          text-align: center;
+  .con {
+    height: calc(100% - 66px);
+    margin-top: 10px;
+    box-sizing: border-box;
+    .nav {
+      width: 100%;
+      display: flex;
+      height: 34px;
+      line-height: 34px;
+      font-size: 16px;
+      background: rgba(19, 39, 165, 0.5);
+      color: rgba(255, 255, 255, 0.8);
+      text-align: center;
 
-          :nth-child(1) {
-              width: 10%;
-          }
-          :nth-child(2) {
-              width: 34%;
-          }
-          :nth-child(3) {
-              width: 33%;
-          }
-          :nth-child(4) {
-              width: 23%;
-          }
+      :nth-child(1) {
+        width: 10%;
       }
-      .content {
-          width: 100%;
-          height: calc(100% - 34px);
+      :nth-child(2) {
+        width: 34%;
+      }
+      :nth-child(3) {
+        width: 33%;
+      }
+      :nth-child(4) {
+        width: 23%;
+      }
+    }
+    .content {
+      width: 100%;
+      height: calc(100% - 34px);
+      overflow: hidden;
+
+      .row {
+        width: 100%;
+        height: 38px;
+        align-items: center;
+        color: #ffffff;
+        font-size: 16px;
+        text-align: center;
+        display: flex;
+
+        &:nth-child(even) {
+          background-color: rgba(19, 39, 165, 0.2);
+        }
+
+        :nth-child(1) {
+          width: 10%;
+        }
+        :nth-child(2) {
+          width: 34%;
+          text-overflow: ellipsis;
+          white-space: nowrap;
           overflow: hidden;
-
-          .row {
-          width: 100%;
-          height: 38px;
-          align-items: center;
-          color: #ffffff;
-          font-size: 16px;
-          text-align: center;
-          display: flex;
-
-              &:nth-child(even) {
-                  background-color: rgba(19, 39, 165, 0.2);
-              }
-
-              :nth-child(1) {
-                  width: 10%;
-              }
-              :nth-child(2) {
-                  width: 34%;
-                  text-overflow: ellipsis;
-                  white-space: nowrap;
-                  overflow: hidden;
-              }
-              :nth-child(3) {
-                  width: 33%;
-                  text-overflow: ellipsis;
-                  white-space: nowrap;
-                  overflow: hidden;
-              }
-              :nth-child(4) {
-                  width: 23%;
-              }
-          }
-
-          .wkg {
-          color: #ff8251;
-          }
-
-          .sgz {
-          color: #00f6ff;
-          }
-
-          .ywg {
-          color: #22ff51;
-          }
+        }
+        :nth-child(3) {
+          width: 33%;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          overflow: hidden;
+        }
+        :nth-child(4) {
+          width: 23%;
+        }
       }
+
+      .wkg {
+        color: #ff8251;
+      }
+
+      .sgz {
+        color: #00f6ff;
+      }
+
+      .ywg {
+        color: #22ff51;
+      }
+    }
   }
 }
 </style>
