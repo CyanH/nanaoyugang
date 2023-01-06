a888e48a7e287690096245f611b2f6eb359c8f2d
Cyan <chenhaiyanccc@163.com>
2023-01-06 08:44:10 +0800

    补充

--- src/views/trawler/map.vue
+++ src/views/trawler/map.vue
@@ -13,46 +13,61 @@ const graphicLayer = new mars3d.layer.GraphicLayer();
 onMounted(() => {
   commonStore.map?.flyHome();
   commonStore.map?.addLayer(graphicLayer);
-  const yaGraphic = new mars3d.graphic.BillboardEntity({
-    position: new mars3d.LngLatPoint(117.094195, 23.408295, 0),
-    style: {
-      image: shipImg,
-      scale: 0.25,
-      clampToGround: true,
-      scaleByDistance: true,
-      scaleByDistance_far: 1000000,
-      scaleByDistance_near: 1,
+
+  let arr = [
+    {
+      lng: 117.094195,
+      lat: 23.408295,
+      name: '粤汕渔05417',
     },
-    attr: { name: '云澳中心渔港' },
-  });
-  graphicLayer.addGraphic(yaGraphic);
-  const wpzGraphic = new mars3d.graphic.BillboardEntity({
-    position: new mars3d.LngLatPoint(117.104857, 23.478669, 0),
-    style: {
-      image: shipImg,
-      scale: 0.25,
-      clampToGround: true,
-      scaleByDistance: true,
-      scaleByDistance_far: 1000000,
-      scaleByDistance_near: 1,
+    {
+      lng: 117.104857,
+      lat: 23.478669,
+      name: '粤汕渔78723',
     },
-    attr: { name: '吴平寨渔港' },
-  });
-  graphicLayer.addGraphic(wpzGraphic);
-  const hjGraphic = new mars3d.graphic.BillboardEntity({
-    position: new mars3d.LngLatPoint(117.025983, 23.445645, 0),
-    style: {
-      image: shipImg,
-      scale: 0.25,
-      clampToGround: true,
-      scaleByDistance: true,
-      scaleByDistance_far: 1000000,
-      scaleByDistance_near: 1,
-      visibleDepth: false,
+    {
+      lng: 117.025983,
+      lat: 23.445645,
+      name: '粤汕渔01388',
     },
-    attr: { name: '后江渔港' },
+    {
+      lng: 117.06321,
+      lat: 23.396924,
+      name: '粤汕渔53242',
+    },
+    {
+      lng: 117.013165,
+      lat: 23.486787,
+      name: '粤汕渔45632',
+    },
+    {
+      lng: 117.027372,
+      lat: 23.473333,
+      name: '粤汕渔63521',
+    },
+    {
+      lng: 116.991423,
+      lat: 23.404527,
+      name: '粤汕渔23156',
+    },
+  ];
+
+  arr.map((it) => {
+    let graphic = new mars3d.graphic.BillboardEntity({
+      position: new mars3d.LngLatPoint(it.lng, it.lat, 0),
+      style: {
+        image: shipImg,
+        scale: 0.25,
+        clampToGround: true,
+        scaleByDistance: true,
+        scaleByDistance_far: 1000000,
+        scaleByDistance_near: 1,
+      },
+      attr: { name: it.name },
+    });
+    graphicLayer.addGraphic(graphic);
   });
-  graphicLayer.addGraphic(hjGraphic);
+
   graphicLayer.bindTooltip((event) => {
     const attr = event.graphic.attr;
     return attr.name;
